024b52e1a41cfed66616bcd6279f0f17c5fe5ee3
AlexxScript <ameneyroalex@gmail.com>
2025-05-10 20:16:36 -0600

    instalador de la app para linux

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+chmod +x sistemagestoarchivos
+sudo mv sistemagestoarchivos /usr/local/bin/
+echo "✅ Instalado en /usr/local/bin"
--- main.go
+++ main.go
@@ -1,3 +1,6 @@
+// https://mattermost-com.translate.goog/blog/building-cli-applications-in-go-with-cobra/?_x_tr_sl=en&_x_tr_tl=es&_x_tr_hl=es&_x_tr_pto=tc
+// https://www.digitalocean.com/community/tutorials/how-to-build-go-executables-for-multiple-platforms-on-ubuntu-16-04
+// https://medium.com/nerd-for-tech/build-cross-platform-executables-in-go-94b84686fb44
 package main
 
 import (
